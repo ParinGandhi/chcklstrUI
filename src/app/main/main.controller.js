5ea422018cefd92e0ef7623dfb0eaad467c24792
Payal Gandhi <Payal Gandhi>
2017-05-11 21:59:30 -0400

    Fixed POST

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,9 +3,10 @@
 
     angular
         .module('chcklstr')
-        .controller('MainController', function MainController(toastr, $modal) {
+        .controller('MainController', function MainController(toastr, $modal, $http, $log) {
             var vm = this;
             vm.chcklst = [];
+            vm.newList = {};
 
             /*vm.testMagic = function () {
     var vanishOut = angular.element(document.querySelector('#testing'));
@@ -22,6 +23,19 @@
                 });
 
                 createNewChklstModalInstance.result.then(function (chcklstName) {
+                    vm.newList.name = chcklstName;
+                    vm.newList.id = 0;
+                    $log.debug(vm.newList);
+                    $http({
+                        method: 'POST',
+                        url: 'http://localhost:8080/checklists/new/',
+                        data: vm.newList
+                    }).then(function (response) {
+                        $log.debug(response);
+                        getAllChecklists();
+                    }, function errorCallback(response) {
+                        $log.debug(response);
+                    });
                     vm.chcklst.push(chcklstName);
                     /*var vanishOut = angular.element(document.querySelector('.testing'));
 vanishOut.removeClass('vanishIn').addClass('vanishIn');*/
@@ -30,5 +44,15 @@ vanishOut.removeClass('vanishIn').addClass('vanishIn');*/
                 });
             };
 
+            function getAllChecklists() {
+                $http.get('http://localhost:8080/checklists/')
+                    .then(function (response) {
+                        vm.chcklst = response.data;
+                    }, function (response) {
+                        $log.debug(response);
+                    });
+            }
+
+            getAllChecklists();
         });
 })();
